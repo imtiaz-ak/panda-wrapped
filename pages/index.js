a7b4b549570090f6f5ca6233ac426ef6ca60a8e6
Imtiaz <ak.imtiaz147@gmail.com>
2022-01-02 14:51:15 +0600

    .

--- pages/index.js
+++ pages/index.js
@@ -49,8 +49,11 @@ export default function Home() {
   }
   if (status!='loading' && !session){
     return (
-      <div className="flex justify-center items-center w-screen h-screen">
-        <h2>Not signed in </h2>
+      <div className="flex flex-col justify-center items-center w-screen h-screen">
+        <p>Welcome to Panda Wrapped</p>
+        <p>The unofficial tool to get your foodpanda wrapped for the year 2021</p>
+        <p>Disclaimer: PandaWrapped has no affiliation with foodpanda. It is a 3rd party tool that uses the foodpanda receipts in your email inbox to create your foodpanda wrapped.</p>
+        <p>In order to get your foodpanda wrapped, you need to verify your gmail address used by your foodpanda account.</p>
         <button onClick={() => signIn('google')}>Sign in</button>
       </div>
     )
