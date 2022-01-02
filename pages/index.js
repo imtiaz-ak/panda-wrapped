3fad78d62177d3dc540ce8ef81c21e5e13454b2a
Imtiaz <ak.imtiaz147@gmail.com>
2022-01-02 13:40:54 +0600

    added tailwindcss support

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { useSession, signIn, signOut, getSession } from "next-auth/react"
 import { useEffect } from 'react'
 import axios from 'axios'
@@ -10,15 +9,25 @@ export default function Home() {
 
   useEffect(async() => {
     if(status != 'loading' && session != undefined){
-      const response = await axios.get(`https://gmail.googleapis.com/gmail/v1/users/me/messages`, {headers: {"Authorization": `Bearer ${session.accessToken}`}})
-      // const response = fetch('https://gmail.googleapis.com/gmail/v1/users/me/profile', { 
-      //   method: 'get', 
-      //   headers: new Headers({
-      //       'Authorization': 'Bearer '+session.accessToken, 
-      //       'Content-Type': 'application/x-www-form-urlencoded'
-      //   }), 
-    // });
-      console.log(response)
+      const QUERY = 'from:(info@mail.foodpanda.com.bd)%20subject:(Your%20order%20has%20been%20placed)%20after:2021/01/01%20before:2021/12/31'
+      const response = await axios.get(`https://gmail.googleapis.com/gmail/v1/users/me/messages?q=${QUERY}`, {headers: {"Authorization": `Bearer ${session.accessToken}`}})
+      let receiptData
+      const receiptArray = await response.data.messages.map(async(item) => {
+        receiptData = await axios.get(`https://gmail.googleapis.com/gmail/v1/users/me/messages/${item.id}`, {headers: {"Authorization": `Bearer ${session.accessToken}`}})
+        return (receiptData.data.payload.parts[1].body.data)
+      })
+
+      let statArray = []
+      let arrLength = receiptArray.length
+      for (let i=0; i < arrLength; i++){
+        console.log(i)
+        receiptArray[i] = await receiptArray[i]
+        // console.log(receiptArray[i])
+        // send receiptArray[i] to a func and populate the statArray with returned data
+        let base64ToString = Buffer.from(receiptArray[i], "base64").toString();
+        statArray.push(base64ToString)
+      }
+      console.log(statArray[0])
     }
     console.log(session)
   }, [status])
@@ -40,10 +49,10 @@ export default function Home() {
   }
   if (status!='loading' && !session){
     return (
-      <>
-        Not signed in <br />
+      <div className="flex justify-center items-center w-screen h-screen">
+        <h2>Not signed in </h2>
         <button onClick={() => signIn('google')}>Sign in</button>
-      </>
+      </div>
     )
   }
 }
