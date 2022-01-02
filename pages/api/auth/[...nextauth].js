2051eb84f89202122735b9269704343e64df5969
Imtiaz <ak.imtiaz147@gmail.com>
2022-01-02 06:56:02 +0600

    initial commit

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        authorization: {
+          params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code",
+            scope: "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/gmail.readonly",
+          }
+        },
+        
+    })
+  ],
+
+  callbacks: {
+    async jwt({ token, account }) {  
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken
+      return session
+    }
+  }
+})
---
+++ pages/api/getEmailData.js
@@ -0,0 +1,23 @@
+import { getToken } from 'next-auth/jwt';
+import axios from 'axios'
+
+const secret = 'ABCDEFG';
+let accessToken;
+
+// // const getData = async(token) => {
+// //   // use axios to get the IDs of the user
+// //   const response = await axios.get(`https://gmail.googleapis.com/gmail/v1/users/me/profile`, {headers: {Authorization: `Bearer ${token}`}})
+// //   return response
+// // }
+
+export default async (req, res) => {
+  console.log('hello world')
+  const data = {name:'home api route'}
+  const token = await getToken({ req, secret });
+
+  // accessToken = token.access_token;
+
+  // const data = await getData(accessToken);
+
+  res.status(200).json(token);
+};
